e7f09d8916b271a05f2174bb4d5a24b420267d3a
janchj <janhernandezj@gmail.com>
2017-09-10 13:14:54 +0100

    included sorting orientation

--- src/controllers/repositories-ctrl.js
+++ src/controllers/repositories-ctrl.js
@@ -6,6 +6,7 @@ const getRepositoriesByHandle = (req, res, next) => {
   const handle = req.swagger.params.handle.value;
   const count = req.swagger.params.count.value;
   const sortBy = req.swagger.params.sortBy.value;
+  const sortOrientation = req.swagger.params.sortOrientation.value;
 
   // get repositories by user handle
   services.github.fetchRepositories(handle)
@@ -16,9 +17,13 @@ const getRepositoriesByHandle = (req, res, next) => {
       };
       if (repos.length > 0) {
         // sort repos by provided key
-        const sortedRepos = shared.utils.sortArrayByKeyDesc(repos, sortBy);
+        const sortedRepos = (sortOrientation === 'asc')
+          ? shared.utils.sortArrayByKeyAsc(repos, sortBy)
+          : shared.utils.sortArrayByKeyDesc(repos, sortBy);
+
         // take the requested # of results
         const filteredRepos = shared.utils.sliceArrayByCount(sortedRepos, count);
+
         // set repositories response
         repoResponse = {
           count: filteredRepos.length,
